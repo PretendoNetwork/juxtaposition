2d4fbcbc673b3ce76f54164fa00c6ac32df18718
Superwhiskers <kspy360@rocketmail.com>
2018-04-13 20:00:20 -0500

    this is the most complicated buildscript i've ever made

--- build.sh
+++ build.sh
@@ -1,23 +1,90 @@
 #!/bin/bash
+# 
+# juxtaposition/build.sh
+# 
+# juxtaposition's buildscript
+# 
+# written by superwhiskers, licensed under gnu agpl.
+# if you want a copy, go to http://www.gnu.org/licenses/
+#
+
+# lists holding the possible arch and os combos
+# example for a new entry: `os:arch`
+# please keep this organized. thanks!
+targets=(
+    "windows:386"
+    "windows:amd64"
+    "linux:arm"
+    "linux:arm64"
+    "linux:386"
+    "linux:amd64"
+    "darwin:386"
+    "darwin:amd64"
+)
 
 # get the bash script directory so nothing is screwed up
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # then fix the running directory
-cd $DIR
+cd $dir
+
+# function to show help
+buildsh_show_help () {
+
+    # show it
+    echo 
+    echo "build.sh [ release | dev | clean ] [ -h | --help ] [ --cross-compile ]"
+    echo 
+    echo "sets up and builds juxtaposition"
+    echo 
+    echo "options:"
+    echo 
+    echo "    -h | --help         shows this"
+    echo "    --cross-compile     when a compilation argument is"
+    echo "                        specified, cross compile it"
+    echo 
+    echo "arguments:"
+    echo
+    echo "    release         build a release package (production)"
+    echo "    dev             build a development package"
+    echo "    clean           clean the build environment"
+    echo 
+
+}
+
+# check if we need to show help
+if [[ "$*" = *"-h"* || "$*" = *"--help"* ]]; then
+
+    # show help
+    buildsh_show_help
 
 # release build
-if [ "$1" == "release" ]; then
+elif [[ "$1" == "release" || "$1" == "dev" ]]; then
 
     # show a message telling what we are doing
     echo "copying config file..."
     
-    # select the appropriate build config to
-    # use
-    cp buildconfs/build.prod.go build.conf.go >/dev/null 2>/dev/null
+    # release build
+    if [ "$1" == "release" ]; then
+
+        # select the appropriate build config to
+        # use
+        cp buildconfs/build.prod.go build.conf.go >/dev/null 2>/dev/null
+        
+        # get the exit code
+        extcode=$?
     
-    # get the exit code
-    extcode=$?
+    # development build
+    elif [ "$1" == "dev" ]; then
+
+        # select the appropriate build
+        # config to use
+        cp buildconfs/build.devel.go build.conf.go >/dev/null 2>/dev/null
+        
+        # get the exit code
+        extcode=$?
+    
+    fi
 
     # if an error occured from the cp command...
     if [ "$extcode" != 0 ]; then
@@ -33,47 +100,51 @@ if [ "$1" == "release" ]; then
     # make the build directory
     mkdir build
 
-    # send a message letting the user know that we have started compiling
-    echo "compiling release build..."
+    # cross-compilation handling
+    if [ "$2" == "--cross-compile" ]; then
 
-    # run the build command
-    go build -buildmode=pie -o build/juxtaposition
+        # send them a message telling them what we are doing
+        echo "compiling $1 build for multiple oses..."
+
+        # start cross compilation
+        for targetstr in "${targets[@]}"
+        do
+
+            # get the target
+            IFS=':' read -r -a target <<< "$targetstr"
+
+            # tell the user what we are compiling for
+            echo 
+            echo "new target:"
+            echo "  os: ${target[0]}"
+            echo "  arch: ${target[1]}"
+            echo
+
+            # -buildmode=pie not supported on windows
+            if [ "${target[0]}" == "windows" ]; then
+
+                # compile it for that windows with that arch
+                env GOOS=${target[0]} GOARCH=${target[1]} go build -o "build/juxtaposition-${target[0]}-${target[1]}.exe"
+
+            else
+            
+                # compile it for that os and arch
+                env GOOS=${target[0]} GOARCH=${target[1]} go build -buildmode=pie -o "build/juxtaposition-${target[0]}-${target[1]}"
+
+            fi
+        
+        done
+
+    else
+
+        # send a message letting the user know that we have started compiling
+        echo "compiling $1 build for host os..."
+
+        # run the build command
+        go build -buildmode=pie -o build/juxtaposition
     
-    # to be continued
-    
-# development build
-elif [ "$1" == "dev" ]; then
-
-    # show a message telling what we are doing
-    echo "copying config file..."
-
-    # select the appropriate build
-    # config to use
-    cp buildconfs/build.devel.go build.conf.go >/dev/null 2>/dev/null
-    
-    # get the exit code
-    extcode=$?
-
-    # if an error occured with the cp command...
-    if [ "$extcode" != 0 ]; then
-
-        # show an error message and the exit code
-        echo "an error occured while copying the config"
-        echo "check if the buildconfs directory exists, and if build.prod.go exists..."
-        echo "exit code of cp: $extcode"
-        exit
-
     fi
-    
-    # make the build directory
-    mkdir build
 
-    # send a message letting the user know that we have started compiling
-    echo "compiling development build..."
-    
-    # run the build command
-    go build -buildmode=pie -o build/juxtaposition
-    
     # to be continued
 
 # clean the environment
@@ -90,19 +161,7 @@ elif [ "$1" == "clean" ]; then
 # show help
 else
 
-    echo 
-    echo "build.sh [ release | dev | clean ] [ -h | --help ]"
-    echo 
-    echo "sets up and builds juxtaposition"
-    echo 
-    echo "options:"
-    echo 
-    echo "    -h | --help     shows this"
-    echo 
-    echo "arguments:"
-    echo "    release         build a release package (production)"
-    echo "    dev             build a development package"
-    echo "    clean           clean the build environment"
-    echo 
+    # function to show help
+    buildsh_show_help
 
 fi
