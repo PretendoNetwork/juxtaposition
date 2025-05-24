092bcf3c73b9230d1bdb732ee86ab17ae95a4d3c
Ash Logan <ash@heyquark.com>
2025-05-24 23:04:10 +1000

    fix(internal): enable posts route
    i'm not stupid i'm smart

--- apps/miiverse-api/src/services/internal/index.ts
+++ apps/miiverse-api/src/services/internal/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { testRouter } from '@/services/internal/routes/test';
+import { postsRouter } from '@/services/internal/routes/posts';
 
 export const internalApiRouter = express.Router();
 internalApiRouter.use('/api/v1', testRouter);
+internalApiRouter.use('/api/v1/posts', postsRouter);
