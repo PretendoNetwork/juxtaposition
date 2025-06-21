8940478ef6678be48768f6b214c497001c0a8577
mrjvs <mistrjvs@gmail.com>
2025-06-21 15:47:55 +0200

    fix: Add html doctype to JSX rendered pages

--- apps/juxtaposition-ui/src/middleware/jsx.ts
+++ apps/juxtaposition-ui/src/middleware/jsx.ts
@@ -1,12 +1,14 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import type { RequestHandler } from 'express';
 
+const htmlDoctype = '<!DOCTYPE html>';
+
 /**
  * Render JSX as static markup. Only static! No state or event handlers are supported.
  */
 export const jsxRenderer: RequestHandler = (_request, response, next) => {
 	response.jsx = (el): typeof response => {
-		response.send(renderToStaticMarkup(el));
+		response.send(htmlDoctype + '\n' + renderToStaticMarkup(el));
 		return response;
 	};
 	next();
