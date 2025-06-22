12e07ed2bff9ca54d4a247d51709406a02a26902
mrjvs <mistrjvs@gmail.com>
2025-06-22 12:25:42 +0200

    feat: Add support for omitting doctype on some rendered jsx pages

--- apps/juxtaposition-ui/src/middleware/jsx.ts
+++ apps/juxtaposition-ui/src/middleware/jsx.ts
@@ -7,25 +7,27 @@ const htmlDoctype = '<!DOCTYPE html>';
  * Render JSX as static markup. Only static! No state or event handlers are supported.
  */
 export const jsxRenderer: RequestHandler = (request, response, next) => {
-	response.jsx = (el): typeof response => {
-		response.send(htmlDoctype + '\n' + renderToStaticMarkup(el));
+	response.jsx = (el, addDoctype): typeof response => {
+		const prefix = addDoctype ? htmlDoctype + '\n' : '';
+		response.send(prefix + renderToStaticMarkup(el));
 		return response;
 	};
 
-	response.jsxForDirectory = (elMap): typeof response => {
+	response.jsxForDirectory = (opt): typeof response => {
+		const disabledFor = opt.disableDoctypeFor ?? [];
 		const directory = request.directory;
-		if (directory === 'ctr') {
-			response.jsx(elMap.ctr);
+		if (directory === 'ctr' && opt.ctr) {
+			response.jsx(opt.ctr, !disabledFor.includes('ctr'));
 			return response;
 		}
 
-		if (directory === 'portal') {
-			response.jsx(elMap.portal);
+		if (directory === 'portal' && opt.portal) {
+			response.jsx(opt.portal, !disabledFor.includes('portal'));
 			return response;
 		}
 
-		if (directory === 'web') {
-			response.jsx(elMap.web);
+		if (directory === 'web' && opt.web) {
+			response.jsx(opt.web, !disabledFor.includes('web'));
 			return response;
 		}
 
--- apps/juxtaposition-ui/src/types/express.d.ts
+++ apps/juxtaposition-ui/src/types/express.d.ts
@@ -1,17 +1,23 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports -- other methods of importing type dont work */
+
 declare namespace Express {
 	export interface Request {
 		directory?: 'ctr' | 'portal' | 'web';
 	}
 
+	export type JsxforDirectoryOptions = Partial<Record<Request['directory'], import('react').ReactElement>> & {
+		disableDoctypeFor?: Request['directory'][];
+	};
+
 	export interface Response {
 		/**
 		 * Render JSX as static markup. Only static! No state or event handlers are supported.
 		 */
-		jsx: (el: ReactElement) => Response;
+		jsx: (el: import('react').ReactElement, addDoctype?: boolean = true) => Response;
 
 		/**
 		 * Render JSX for directory set on req.directory as static markup. Only static! No state or event handlers are supported.
 		 */
-		jsxForDirectory: (el: Partial<Record<Request['directory'], ReactElement>>) => Response;
+		jsxForDirectory: (el: JsxforDirectoryOptions) => Response;
 	}
 }
