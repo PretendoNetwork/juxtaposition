b81cc1426cfa687dfddd1af0aa54754ef3594476
Ash Logan <ash@heyquark.com>
2025-06-30 22:46:10 +1000

    feat(internal): Implement /posts?{topic_tag,posted_by,empathy_by}

--- apps/miiverse-api/src/services/internal/routes/posts.ts
+++ apps/miiverse-api/src/services/internal/routes/posts.ts
@@ -1,17 +1,48 @@
 import express from 'express';
-import { getPostByID } from '@/database';
+import * as z from 'zod/v4';
+import { Post } from '@/models/post';
 import { errors } from '@/services/internal/errors';
-import { handle } from '@/services/internal/utils';
+import { deleteOptional, filterRemovedPosts, handle } from '@/services/internal/utils';
 import { guards } from '@/services/internal/middleware/guards';
 import { mapPost } from '@/services/internal/contract/post';
 
 export const postsRouter = express.Router();
 
+// Get posts by topic tag, poster, or empathy
+// TODO paging
+postsRouter.get('/posts', guards.user, handle(async ({ req, res }) => {
+	// the idea is that any combination of these can be left undefined
+	const query = z.object({
+		topic_tag: z.string().optional(),
+		posted_by: z.coerce.number().optional(),
+		empathy_by: z.coerce.number().optional()
+	}).parse(req.query);
+
+	const posts = await Post.find(deleteOptional({
+		pid: query.posted_by,
+		topic_tag: query.topic_tag,
+		yeahs: query.empathy_by,
+		...filterRemovedPosts(res.locals.account)
+	})).sort({ created_at: -1 }).limit(10); // TODO configure postLimit?
+
+	// PostDto[]
+	return posts.map(mapPost);
+}));
+
+// Get post by id
 postsRouter.get('/posts/:post_id', guards.guest, handle(async ({ req, res }) => {
-	const post = await getPostByID(req.params.post_id);
-	if (!post || (post.removed && res.locals.account?.moderator !== true)) {
+	const params = z.object({
+		post_id: z.string().length(21)
+	}).parse(req.params);
+
+	const post = await Post.findOne({
+		id: params.post_id,
+		...filterRemovedPosts(res.locals.account)
+	});
+	if (!post) {
 		throw new errors.notFound('Post not found');
 	}
 
+	// PostDto
 	return mapPost(post);
 }));
