1481d802ae75c2550b5e840084a277167c4691f0
mrjvs <jellevs@gmail.com>
2025-10-02 20:37:27 +0200

    feat: add deletion process to miiverse content

--- apps/miiverse-api/src/services/grpc/server.ts
+++ apps/miiverse-api/src/services/grpc/server.ts
@@ -1,10 +1,9 @@
 import { ServerError, Status } from 'nice-grpc';
+import { deleteAccountData } from '@/services/grpc/deleteAccountData';
 import type { MiiverseServiceImplementation } from '@pretendonetwork/grpc/miiverse/v2/miiverse_service';
 
 export const miiverseDefinition: MiiverseServiceImplementation = {
-	deleteAccountData: async (_request) => {
-		throw new ServerError(Status.UNIMPLEMENTED, 'Not implemented');
-	},
+	deleteAccountData,
 	sMMRequestPostId: async (_request) => {
 		throw new ServerError(Status.UNIMPLEMENTED, 'Not implemented');
 	}
