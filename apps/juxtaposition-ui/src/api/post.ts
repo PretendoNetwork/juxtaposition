a91c5a5a6a240159399cb6fc8ca46c51b6c5fef9
Ash Logan <ash@heyquark.com>
2025-10-16 14:40:53 +1100

    fix: switch to new query string helper

--- apps/juxtaposition-ui/src/api/post.ts
+++ apps/juxtaposition-ui/src/api/post.ts
@@ -1,5 +1,4 @@
 import { apiFetchUser } from '@/fetch';
-import { deleteOptional } from '@/util';
 import type { ResultDto } from '@/api/result';
 import type { PageDto } from '@/api/page';
 import type { UserTokens } from '@/fetch';
@@ -70,38 +69,43 @@ export async function getPostById(tokens: UserTokens, post_id: string): Promise<
 }
 
 export async function getPostsByPoster(tokens: UserTokens, poster_pid: number, offset: number): Promise<PageDto<PostDto> | null> {
-	const params = new URLSearchParams({
-		posted_by: poster_pid.toString(),
-		offset: offset.toString()
+	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts`, {
+		query: {
+			posted_by: poster_pid,
+			offset
+		}
 	});
-	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts?${params}`);
 	return posts;
 }
 
 export async function getPostsByEmpathy(tokens: UserTokens, empathy_by: number, offset: number): Promise<PageDto<PostDto> | null> {
-	const params = new URLSearchParams({
-		empathy_by: empathy_by.toString(),
-		offset: offset.toString(),
-		include_replies: 'true'
+	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts`, {
+		query: {
+			empathy_by,
+			offset,
+			include_replies: 'true'
+		}
 	});
-	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts?${params}`);
 	return posts;
 }
 
 export async function getPostsByParentId(tokens: UserTokens, parent_id: string, offset: number): Promise<PageDto<PostDto> | null> {
-	const params = new URLSearchParams({
-		parent_id: parent_id,
-		offset: offset.toString(),
-		include_replies: 'true'
+	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts`, {
+		query: {
+			parent_id: parent_id,
+			offset: offset.toString(),
+			include_replies: 'true'
+		}
 	});
-	const posts = await apiFetchUser<PageDto<PostDto>>(tokens, `/api/v1/posts?${params}`);
 	return posts;
 }
 
 export async function deletePostById(tokens: UserTokens, post_id: string, reason?: string): Promise<ResultDto | null> {
-	const params = new URLSearchParams(deleteOptional({
-		reason
-	}));
-	const result = apiFetchUser<ResultDto>(tokens, `/api/v1/posts/${post_id}?${params}`, { method: 'DELETE' });
+	const result = apiFetchUser<ResultDto>(tokens, `/api/v1/posts/${post_id}`, {
+		method: 'DELETE',
+		query: {
+			reason
+		}
+	});
 	return result;
 }
