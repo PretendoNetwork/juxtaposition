f180cb7b0276fa6318e3e9c42e4a2955bd9a70df
mrjvs <jellevs@gmail.com>
2026-01-04 11:59:38 +0100

    chore: make toast animations play on login page as well

--- apps/juxtaposition-ui/src/webfiles/web/js/login.js
+++ apps/juxtaposition-ui/src/webfiles/web/js/login.js
@@ -1,10 +1,32 @@
-function hideToast() {
-	const toast = document.getElementById('toast');
-	window.setTimeout(function () {
-		toast.className = toast.className.replace('show', '');
-	}, 20000);
+// Toast code duplicated at web.js
+function Toast(text) {
+	const x = document.getElementById('toast');
+	x.innerText = text;
+	x.className = 'show';
+	startHideToast(3000);
+}
+
+function initToast() {
+	const x = document.getElementById('toast');
+	if (!x) {
+		return; // No toast on screen
+	}
+	const attr = x.getAttribute('data-show');
+	if (!attr) {
+		return; // Nothing to do
+	}
+
+	x.removeAttribute('data-show');
+	setTimeout(() => Toast(x.innerText), 100); // Show after a delay so it shows an animation
+}
+
+function startHideToast(ms) {
+	setTimeout(function () {
+		const x = document.getElementById('toast');
+		x.className = x.className.replace('show', '');
+	}, ms);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	hideToast();
+	initToast();
 });
