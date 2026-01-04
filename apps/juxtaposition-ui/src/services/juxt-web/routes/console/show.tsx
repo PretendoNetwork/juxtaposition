1464671bcb3f697c354ff2dcfc7b7c049b45a2df
mrjvs <jellevs@gmail.com>
2026-01-04 13:18:56 +0100

    feat: port show.js to typescript

---
+++ apps/juxtaposition-ui/src/services/juxt-web/routes/console/show.tsx
@@ -0,0 +1,76 @@
+import express from 'express';
+import z from 'zod';
+import { database } from '@/database';
+import { createUser, setName } from '@/util';
+import { parseReq } from '@/services/juxt-web/routes/routeUtils';
+
+export const showRouter = express.Router();
+
+showRouter.get('/', async function (req, res) {
+	const { auth, hasAuth, query } = parseReq(req, {
+		query: z.object({
+			topic_tag: z.string().optional(),
+			pid: z.string().optional()
+		})
+	});
+
+	if (req.pid === 1000000000) {
+		return res.render(req.directory + '/guest_notice.ejs');
+	}
+
+	const user = await database.getUserSettings(req.pid);
+	const content = await database.getUserContent(req.pid);
+	if (!user || !content) {
+		return res.render(req.directory + '/first_run.ejs');
+	}
+
+	if (query.topic_tag) {
+		res.redirect(`/topics?topic_tag=${query.topic_tag}`);
+	} else if (query.pid) {
+		res.redirect(`/users/${query.pid}`);
+	} else {
+		res.redirect('/titles');
+	}
+
+	if (hasAuth()) {
+		const currentMii = auth().user.mii;
+		const usrMii = await database.getUserSettings(req.pid);
+		if (!currentMii || !usrMii) {
+			return;
+		}
+		if (currentMii.name !== usrMii.screen_name) {
+			setName(auth().pid, currentMii.name);
+			usrMii.screen_name = currentMii.name;
+			await usrMii.save();
+		}
+	}
+});
+
+showRouter.get('/first', async function (req, res) {
+	res.render(req.directory + '/first_run.ejs');
+});
+
+showRouter.post('/newUser', async function (req, res) {
+	const { auth, body } = parseReq(req, {
+		body: z.object({
+			experience: z.number(),
+			notifications: z.boolean()
+		})
+	});
+
+	if (req.pid === null || !req.new_users || req.directory === 'web') {
+		return res.sendStatus(401);
+	}
+
+	const user = await database.getUserSettings(auth().pid);
+	if (user) {
+		return res.sendStatus(504); // User already exists
+	}
+
+	await createUser(auth().pid, body.experience, body.notifications);
+	if (await database.getUserSettings(auth().pid) !== null) {
+		res.sendStatus(200);
+	} else {
+		res.sendStatus(504);
+	}
+});
