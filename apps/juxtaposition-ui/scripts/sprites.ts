a29e15577d426708f90e0ba287828f60a85fb212
Ash Logan <ash@heyquark.com>
2026-01-09 11:15:31 +1100

    feat: add spritesheet script
    still unsure if this should be an ESBuild plugin or just run manually

---
+++ apps/juxtaposition-ui/scripts/sprites.ts
@@ -0,0 +1,49 @@
+// Load in dependencies
+import { readdirSync, writeFileSync } from 'node:fs';
+import { basename } from 'node:path';
+import Spritesmith from 'spritesmith';
+// @ts-expect-error: missing upstream types
+import templater from 'spritesheet-templates';
+
+const ctr = 'src/webfiles/ctr';
+const ctr_images = ctr + '/images';
+const sprite_paths = readdirSync(ctr_images + '/sprites')
+	.filter(path => path.endsWith('.png'))
+	.map(path => ctr_images + '/sprites/' + path);
+
+// Generate our spritesheet
+Spritesmith.run({
+	src: sprite_paths
+}, function handleResult(err, result) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	const { coordinates, properties, image } = result;
+
+	const sprites = Object.entries(coordinates).map(([name, coords]) => {
+		return {
+			name: basename(name, '.png'),
+			...coords
+		};
+	});
+
+	const res = templater({
+		sprites,
+		spritesheet: {
+			...properties,
+			image: '/images/sprites.png'
+		}
+	}, {
+		format: 'css',
+		formatOpts: {
+			cssSelector: ({ name }: { name: string }) => '.sprite-' + name
+		}
+	});
+
+	writeFileSync(ctr + '/css/sprites.css', res, {
+		encoding: 'utf-8'
+	});
+	writeFileSync(ctr + '/images/sprites.png', image);
+	console.info(`success: wrote ${sprite_paths.length} sprites`);
+});
