f573680839440ddd8a57314ad3195e98b2200357
Ash Logan <ash@heyquark.com>
2026-02-18 19:42:36 +1100

    feat(ctr): Add yeah list to post view

--- apps/juxtaposition-ui/src/services/juxt-web/views/ctr/post.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/ctr/post.tsx
@@ -135,7 +135,22 @@ export function CtrPostView(props: PostViewProps): ReactNode {
 					</div>
 				</div>
 			</div>
-			{/* TODO add locals.yeah logic back */}
+			{ props.isMainPost && post.yeahs.length > 0
+				? (
+						<>
+							<h6 className="yeah-text">
+								<span className="feeling">{ post.yeahs.length }</span>
+								{' '}
+								people gave this post a yeah.
+							</h6>
+							<div className="yeah-list">
+								{post.yeahs.slice(0, 10).map(pid => (
+									<CtrMiiIcon ctx={props.ctx} pid={pid}></CtrMiiIcon>
+								))}
+							</div>
+						</>
+					)
+				: null}
 		</div>
 	);
 }
