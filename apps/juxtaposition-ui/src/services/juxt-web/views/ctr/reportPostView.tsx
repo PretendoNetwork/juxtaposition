23dd6d5b69326350a1ef6235f30a88028aa5bf7b
Ash Logan <ash@heyquark.com>
2026-02-27 23:49:37 +1100

    fix(ctr): Tidy up reporting page

--- apps/juxtaposition-ui/src/services/juxt-web/views/ctr/reportPostView.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/ctr/reportPostView.tsx
@@ -4,36 +4,44 @@ import type { ReportPostViewProps } from '@/services/juxt-web/views/web/reportPo
 
 export function CtrReportPostView(props: ReportPostViewProps): ReactNode {
 	return (
-		<div id="report-post-page" className="add-post-page official-user-post">
-			<header className="add-post-page-header" id="header">
+		<div id="report-post-page">
+			<header
+				id="header"
+				data-toolbar-mode="wide"
+				data-toolbar-message="Submit Report"
+			>
 				<h1 id="page-title">Report Post</h1>
 			</header>
-			<form method="post" action={`/posts/${props.id}/report`} id="report-form" className="post" name="report" data-is-own-title="1" data-is-identified="1">
+			<form method="post" action={`/posts/${props.id}/report`} className="report-form post" name="report">
 				<input type="hidden" name="post_id" id="report-post-id" value={props.id} />
-				<div className="add-post-page-content report">
-					<p>
-						You are about to report a post with content which violates the Juxtaposition Code of Conduct.
-						This report will be sent to Pretendo's Juxtaposition administrators and not to the creator of the post.
-					</p>
-					<div>
-						<h4>Violation Type:</h4>
-						<select name="reason" id="report">
-							<option value="0">Spoiler</option>
-							<option value="1">Personal Information</option>
-							<option value="2">Violent Content</option>
-							<option value="3">Inappropriate/Harmful Conduct</option>
-							<option value="4">Hateful/Bullying</option>
-							<option value="5">Advertising</option>
-							<option value="6">Sexually Explicit</option>
-							<option value="7">Piracy</option>
-							<option value="8">Inappropriate Behavior in Game</option>
-							<option value="10">Missing Images</option>
-							<option value="9">Other</option>
-						</select>
-					</div>
-					<textarea name="message" className="textarea-text" value="" maxLength={280} placeholder="Enter additional comments or information"></textarea>
+				<p>
+					You are about to report a post with content which violates the Juxtaposition Code of Conduct.
+					This report will be sent to Pretendo's Juxtaposition administrators and not to the creator of the post.
+				</p>
+				<div className="dropdown">
+					<select name="reason" id="report">
+						<option value="" disabled selected>Select violation type...</option>
+						<option value="0">Spoiler</option>
+						<option value="1">Personal Information</option>
+						<option value="2">Violent Content</option>
+						<option value="3">Inappropriate/Harmful Conduct</option>
+						<option value="4">Hateful/Bullying</option>
+						<option value="5">Advertising</option>
+						<option value="6">Sexually Explicit</option>
+						<option value="7">Piracy</option>
+						<option value="8">Inappropriate Behavior in Game</option>
+						<option value="10">Missing Images</option>
+						<option value="9">Other</option>
+					</select>
 				</div>
-				<input type="submit" className="post-button fixed-bottom-button" value="Submit" />
+				<textarea
+					name="message"
+					maxLength={280}
+					rows={4}
+					placeholder="Enter additional comments or information"
+				>
+				</textarea>
+				<input type="submit" id="submit" />
 			</form>
 		</div>
 	);
