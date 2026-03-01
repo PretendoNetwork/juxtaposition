9e2cd7b463807bd62f126ccbd2bd391c905c039b
mrjvs <jellevs@gmail.com>
2026-03-01 13:21:29 +0100

    fix: add simple static route back in for notification images

--- apps/juxtaposition-ui/src/services/juxt-web/routes/console/static.ts
+++ apps/juxtaposition-ui/src/services/juxt-web/routes/console/static.ts
@@ -8,3 +8,11 @@ staticRouter.use('/assets', express.static(webFilesRoot));
 
 const webFilesGlobalRoot = path.join(distFolder, 'webfiles', 'global');
 staticRouter.use('/', express.static(webFilesGlobalRoot));
+
+// Some notification database entries use hardcoded paths to images, will need a migration.
+// Once that has been updated, this route can be removed
+staticRouter.use('/images', (req, res, next) => {
+	const directory = req.directory ?? 'web';
+	const fileRoot = path.join(distFolder, 'webfiles', directory, 'images');
+	return express.static(fileRoot)(req, res, next);
+});
