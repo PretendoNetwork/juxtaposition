PretendoNetwork/juxtaposition

commit dbb0ca0a3ccc7f0f940d0eadbca32ba790c0bdbf

fix(ui): follow button, use classList polyfill

apps/juxtaposition-ui/webfiles/ctr/js/juxt.js
@@ -6,7 +6,6 @@ import { initNewPostView } from './new-post-view';
 import { pjaxBack, pjaxCanGoBack, pjaxInit, pjaxLoadUrl, pjaxRefresh } from './pjax';
 import { initPostPageView, initYeahButton } from './post';
 import { initToolbarConfigs } from './toolbar';
-import { classList } from './util';
 import { GET, POST } from './xhr';
 
 setInterval(checkForUpdates, 30000);
@@ -77,13 +76,11 @@ function initSpoilers() {
 	for (var i = 0; i < els.length; i++) {
 		els[i].addEventListener('click', function (e) {
 			var el = e.currentTarget;
-			classList.remove(
-				document.getElementById('post-' + el.getAttribute('data-post-id')),
+			var target = document.getElementById('post-' + el.getAttribute('data-post-id'));
+			target.classList.remove(
 				'spoiler'
 			);
-			document.getElementById(
-				'spoiler-' + el.getAttribute('data-post-id')
-			).outerHTML = '';
+			target.outerHTML = '';
 			cave.snd_playSe('SE_OLV_OK');
 		});
 	}
@@ -103,11 +100,9 @@ function initTabs() {
 		var child = el.children[0];
 
 		for (var i = 0; i < els.length; i++) {
-			if (classList.contains(els[i], 'selected')) {
-				classList.remove(els[i], 'selected');
-			}
+			els[i].classList.remove('selected');
 		}
-		classList.add(el, 'selected');
+		el.classList.add('selected');
 
 		GET(child.getAttribute('href') + '?pjax=true', function a(data) {
 			var response = data.responseText;
@@ -166,14 +161,13 @@ function checkForUpdates() {
 function follow(el) {
 	var id = el.getAttribute('data-community-id');
 	var count = document.getElementById('followers');
+	var sprite = el.querySelector('.sprite.sp-yeah');
 	el.disabled = true;
 	var params = 'id=' + id;
-	if (classList.contains(el, 'selected')) {
-		classList.remove(el, 'selected');
-		cave.snd_playSe('SE_OLV_CANCEL');
-	} else {
-		classList.add(el, 'selected');
+	if (sprite.classList.toggle('selected')) {
 		cave.snd_playSe('SE_OLV_MII_ADD');
+	} else {
+		cave.snd_playSe('SE_OLV_CANCEL');
 	}
 
 	POST(el.getAttribute('data-url'), params, function a(data) {

apps/juxtaposition-ui/src/services/juxt-web/views/ctr/communityView.tsx
@@ -76,15 +76,19 @@ export function CtrCommunityView(props: CommunityViewProps): ReactNode {
 								<button
 									type="button"
 									className={cx('small-button follow', {
-										suggested: props.hasSubCommunities,
-										selected: props.isUserFollowing
+										suggested: props.hasSubCommunities
+
 									})}
 									evt-click="follow(this)"
 									data-sound="SE_WAVE_CHECKBOX_UNCHECK"
 									data-url="/titles/follow"
 									data-community-id={community.olive_community_id}
 								>
-									<span className="sprite sp-yeah inline-sprite"></span>
+									<span className={cx('sprite sp-yeah inline-sprite', {
+										selected: props.isUserFollowing
+									})}
+									>
+									</span>
 								</button>
 							)
 						: null}

apps/juxtaposition-ui/src/services/juxt-web/views/ctr/userPageView.tsx
@@ -73,8 +73,8 @@ export function CtrUserPageView(props: UserPageViewProps): ReactNode {
 					{isSelf ? <a id="header-communities-button" className="header-button left" href="/users/me/settings" data-pjax="#body"><T k="user_page.settings" /></a> : null}
 					{ canViewUser && !isSelf
 						? (
-								<button type="button" className={cx('small-button follow', { selected: isRequesterFollowingUser })} evt-click="follow(this)" data-sound="SE_WAVE_CHECKBOX_UNCHECK" data-url="/users/follow" data-community-id={props.user.pid}>
-									<span className="sprite sp-yeah inline-sprite"></span>
+								<button type="button" className="small-button follow" evt-click="follow(this)" data-sound="SE_WAVE_CHECKBOX_UNCHECK" data-url="/users/follow" data-community-id={props.user.pid}>
+									<span className={cx('sprite sp-yeah inline-sprite', { selected: isRequesterFollowingUser })}></span>
 								</button>
 							)
 						: null}

apps/juxtaposition-ui/webfiles/ctr/js/polyfills/Element.prototype.classList.js
@@ -1,5 +1,5 @@
 // Element.prototype.classList by Remy Sharp
-// updated by thednp
+// updated by thednp, ashquarky
 
 var ClassLIST = function (elem) {
 	var classArr = (elem.getAttribute('class') || '').trim().split(/\s+/) || [];
@@ -23,8 +23,10 @@ var ClassLIST = function (elem) {
 	this.toggle = function (classNAME) {
 		if (this.contains(classNAME)) {
 			this.remove(classNAME);
+			return false;
 		} else {
 			this.add(classNAME);
+			return true;
 		}
 	};
 };

apps/juxtaposition-ui/webfiles/ctr/js/post.ts
@@ -1,6 +1,5 @@
 // Script for the post page view (postPageView.tsx)
 import { deletePostById, empathyPostById } from './api';
-import { classList } from './util';
 
 function deletePost(this: HTMLElement, _e: Event): void {
 	var id = this.getAttribute('data-button-delete-post');
@@ -50,7 +49,7 @@ function yeahPost(this: HTMLInputElement, _e: Event): void {
 	var count = document.getElementById('count-' + id)!;
 	this.disabled = true;
 
-	if (classList.toggle(sprite, 'selected')) {
+	if (sprite.classList.toggle('selected')) {
 		count.innerText = inc(count.innerText, 1);
 		// @ts-expect-error incorrect upstream types for SE label
 		cave.snd_playSe('SE_OLV_MII_ADD');

apps/juxtaposition-ui/webfiles/ctr/js/util.ts
@@ -1,20 +1,0 @@
-export var classList = {
-	contains: function (el: Element, string: string): boolean {
-		return el.className.indexOf(string) !== -1;
-	},
-	add: function (el: Element, string: string): void {
-		el.className += ' ' + string;
-	},
-	remove: function (el: Element, string: string): void {
-		el.className = el.className.replace(string, '');
-	},
-	toggle: function (el: Element, string: string): boolean {
-		if (!this.contains(el, string)) {
-			this.add(el, string);
-			return true;
-		} else {
-			this.remove(el, string);
-			return false;
-		}
-	}
-};

commit aa7fb46820ad03d6aa7d42dd83ba3bbd98fcda58

fix(ctr): remove spoiler button, not the whole dang post
Fixes: dbb0ca0a3ccc7f0f940d0eadbca32ba790c0bdbf

apps/juxtaposition-ui/webfiles/ctr/js/juxt.js
@@ -80,7 +80,7 @@ function initSpoilers() {
 			target.classList.remove(
 				'spoiler'
 			);
-			target.outerHTML = '';
+			el.outerHTML = '';
 			cave.snd_playSe('SE_OLV_OK');
 		});
 	}