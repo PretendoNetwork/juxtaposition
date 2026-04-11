b177351b6c0db82309f209ee9a49c3af2a51a41e
mrjvs <jellevs@gmail.com>
2026-04-11 14:33:29 +0200

    chore: remove test router

--- apps/miiverse-api/src/services/internal/index.ts
+++ apps/miiverse-api/src/services/internal/index.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import { testRouter } from '@/services/internal/routes/test';
 import { postsRouter } from '@/services/internal/routes/posts';
 
 export const internalApiRouter = express.Router();
-internalApiRouter.use('/api/v1', testRouter);
 internalApiRouter.use('/api/v1', postsRouter);
