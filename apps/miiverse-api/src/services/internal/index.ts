3ad33062fb337e4a19a57884bffb1e2a23e823ec
mrjvs <jellevs@gmail.com>
2026-04-12 19:35:14 +0200

    feat: add endpoint to list users posts

--- apps/miiverse-api/src/services/internal/index.ts
+++ apps/miiverse-api/src/services/internal/index.ts
@@ -3,9 +3,11 @@ import { postsRouter } from '@/services/internal/routes/posts';
 import { communitiesRouter } from '@/services/internal/routes/communities';
 import { activityFeedsRouter } from '@/services/internal/routes/activityFeeds';
 import { communityPostsRouter } from '@/services/internal/routes/communityPosts';
+import { userPostsRouter } from '@/services/internal/routes/userPosts';
 
 export const internalApiRouter = express.Router();
 internalApiRouter.use('/api/v1', postsRouter.toRouter());
 internalApiRouter.use('/api/v1', communitiesRouter.toRouter());
 internalApiRouter.use('/api/v1', activityFeedsRouter.toRouter());
 internalApiRouter.use('/api/v1', communityPostsRouter.toRouter());
+internalApiRouter.use('/api/v1', userPostsRouter.toRouter());
