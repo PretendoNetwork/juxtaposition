e7e23a07e133d9861d958831b6ec74240beb9213
mrjvs <jellevs@gmail.com>
2026-07-06 00:09:40 +0200

    feat: clean up notification rendering into separate components

--- apps/juxtaposition-ui/src/services/juxt-web/views/portal/notificationListView.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/portal/notificationListView.tsx
@@ -6,156 +6,174 @@ import { PortalMiiIcon } from '@/services/juxt-web/views/portal/components/ui/Po
 import { PortalIcon } from '@/services/juxt-web/views/portal/components/ui/PortalIcon';
 import type { ReactNode } from 'react';
 import type { TranslationKey } from '@/services/juxt-web/views/common/components/T';
-import type { NotificationItemProps, NotificationListViewProps, NotificationWrapperViewProps } from '@/services/juxt-web/views/web/notificationListView';
-import type { ShallowUser } from '@/api/generated';
+import type { NotificationItemProps, NotificationItemTypeProps, NotificationListViewProps, NotificationWrapperViewProps } from '@/services/juxt-web/views/web/notificationListView';
+import type { FollowNotification, LimitedFromPostingNotification, PostDeletedNotification, ShallowUser, SystemNotification } from '@/api/generated';
 
-function PortalNotificationItem(props: NotificationItemProps): ReactNode {
-	const data = props.notification;
-	if (data.notif.type === 'follow') {
-		const NickName = ({ user }: { user: ShallowUser | null | undefined }): ReactNode => <span className="nick-name">{user?.miiName ?? null}</span>;
-		const users = [...data.notif.content.users].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
-		const latestUser = users[0];
+function FollowNotificationView(props: NotificationItemTypeProps<FollowNotification>): ReactNode {
+	const NickName = ({ user }: { user: ShallowUser | null | undefined }): ReactNode => <span className="nick-name">{user?.miiName ?? null}</span>;
+	const users = [...props.notif.content.users].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+	const latestUser = users[0];
 
-		let i18nKey: TranslationKey = 'notifications.new_follower.message/one';
-		if (users.length === 2) {
-			i18nKey = 'notifications.new_follower.message/two';
-		}
-		if (users.length === 3) {
-			i18nKey = 'notifications.new_follower.message/three';
-		}
-		if (users.length > 3) {
-			i18nKey = 'notifications.new_follower.message/multiple';
-		}
+	let i18nKey: TranslationKey = 'notifications.new_follower.message/one';
+	if (users.length === 2) {
+		i18nKey = 'notifications.new_follower.message/two';
+	}
+	if (users.length === 3) {
+		i18nKey = 'notifications.new_follower.message/three';
+	}
+	if (users.length > 3) {
+		i18nKey = 'notifications.new_follower.message/multiple';
+	}
 
-		return (
-			<>
-				<PortalMiiIcon pid={latestUser.pid} type="icon"></PortalMiiIcon>
-				<div className="body">
-					<p className="text">
-						<a className="link" href={`/users/${latestUser.pid}`}>
-							<T
-								k={i18nKey}
-								values={{
-									count: users.length,
-									count_other: Math.max(0, users.length - 2)
-								}}
-								components={{
-									follower_one: <NickName user={users[0]?.user} />,
-									follower_two: <NickName user={users[1]?.user} />
-								}}
-							/>
-						</a>
+	return (
+		<>
+			<PortalMiiIcon pid={latestUser.pid} type="icon"></PortalMiiIcon>
+			<div className="body">
+				<p className="text">
+					<a className="link" href={`/users/${latestUser.pid}`}>
+						<T
+							k={i18nKey}
+							values={{
+								count: users.length,
+								count_other: Math.max(0, users.length - 2)
+							}}
+							components={{
+								follower_one: <NickName user={users[0]?.user} />,
+								follower_two: <NickName user={users[1]?.user} />
+							}}
+						/>
+					</a>
+					<span className="timestamp">
+						{' '}
+						{humanFromNow(props.data.updatedAt)}
+					</span>
+				</p>
+			</div>
+		</>
+	);
+}
+
+function PostDeletedNotificationView(props: NotificationItemTypeProps<PostDeletedNotification>): ReactNode {
+	let i18nKey: TranslationKey = 'notifications.post_deleted.post_removed';
+	if (props.notif.content.reason) {
+		i18nKey = 'notifications.post_deleted.post_removed_for_reason';
+	}
+	if (props.notif.content.postType === 'comment') {
+		i18nKey = 'notifications.post_deleted.comment_removed';
+		if (props.notif.content.reason) {
+			i18nKey = 'notifications.post_deleted.comment_removed_for_reason';
+		}
+	}
+
+	return (
+		<>
+			<PortalIcon href="/titles/2551084080/new" src="/images/bandwidthalert.png"></PortalIcon>
+			<div className="body">
+				<a href="/titles/2551084080/new">
+					<span className="text">
+						<T
+							k={i18nKey}
+							values={{
+								postId: props.notif.content.postId,
+								reason: props.notif.content.reason ?? ''
+							}}
+						/>
+						{' '}
+						<T
+							k="notifications.post_deleted.footer"
+							values={{
+								contactModsUrl: 'https://preten.do/juxt-mods/'
+							}}
+						/>
 						<span className="timestamp">
 							{' '}
-							{humanFromNow(data.updatedAt)}
+							{humanFromNow(props.data.updatedAt)}
 						</span>
-					</p>
-				</div>
-			</>
-		);
+					</span>
+				</a>
+			</div>
+		</>
+	);
+}
+
+function LimitedFromPostingNotificationView(props: NotificationItemTypeProps<LimitedFromPostingNotification>): ReactNode {
+	let i18nKey: TranslationKey = 'notifications.limited_from_posting.message';
+	if (props.notif.content.reason) {
+		i18nKey = 'notifications.limited_from_posting.message_with_reason';
+	}
+	if (props.notif.content.until) {
+		i18nKey = 'notifications.limited_from_posting.temporary';
+		if (props.notif.content.reason) {
+			i18nKey = 'notifications.limited_from_posting.temporary_with_reason';
+		}
 	}
 
-	if (data.notif.type === 'postDeleted') {
-		let i18nKey: TranslationKey = 'notifications.post_deleted.post_removed';
-		if (data.notif.content.reason) {
-			i18nKey = 'notifications.post_deleted.post_removed_for_reason';
-		}
-		if (data.notif.content.postType === 'comment') {
-			i18nKey = 'notifications.post_deleted.comment_removed';
-			if (data.notif.content.reason) {
-				i18nKey = 'notifications.post_deleted.comment_removed_for_reason';
-			}
-		}
-		return (
-			<>
-				<PortalIcon href="/titles/2551084080/new" src="/images/bandwidthalert.png"></PortalIcon>
-				<div className="body">
-					<a href="/titles/2551084080/new">
-						<span className="text">
-							<T
-								k={i18nKey}
-								values={{
-									postId: data.notif.content.postId,
-									reason: data.notif.content.reason ?? ''
-								}}
-							/>
+	return (
+		<>
+			<PortalIcon href="/titles/2551084080/new" src="/images/bandwidthalert.png"></PortalIcon>
+			<div className="body">
+				<a href="/titles/2551084080/new">
+					<span className="text">
+						<T
+							k={i18nKey}
+							values={{
+								until: props.notif.content.until ? humanDate(props.notif.content.until) : '',
+								reason: props.notif.content.reason ?? ''
+							}}
+						/>
+						{' '}
+						<T
+							k="notifications.limited_from_posting.footer"
+							values={{
+								banAppealUrl: 'https://preten.do/ban-appeal/'
+							}}
+						/>
+						<span className="timestamp">
 							{' '}
-							<T
-								k="notifications.post_deleted.footer"
-								values={{
-									contactModsUrl: 'https://preten.do/juxt-mods/'
-								}}
-							/>
-							<span className="timestamp">
-								{' '}
-								{humanFromNow(data.updatedAt)}
-							</span>
+							{humanFromNow(props.data.updatedAt)}
 						</span>
-					</a>
-				</div>
-			</>
-		);
-	}
+					</span>
+				</a>
+			</div>
+		</>
+	);
+}
 
-	if (data.notif.type === 'limitedFromPosting') {
-		let i18nKey: TranslationKey = 'notifications.limited_from_posting.message';
-		if (data.notif.content.reason) {
-			i18nKey = 'notifications.limited_from_posting.message_with_reason';
-		}
-		if (data.notif.content.until) {
-			i18nKey = 'notifications.limited_from_posting.temporary';
-			if (data.notif.content.reason) {
-				i18nKey = 'notifications.limited_from_posting.temporary_with_reason';
-			}
-		}
-		return (
-			<>
-				<PortalIcon href="/titles/2551084080/new" src="/images/bandwidthalert.png"></PortalIcon>
-				<div className="body">
-					<a href="/titles/2551084080/new">
-						<span className="text">
-							<T
-								k={i18nKey}
-								values={{
-									until: data.notif.content.until ? humanDate(data.notif.content.until) : '',
-									reason: data.notif.content.reason ?? ''
-								}}
-							/>
+function SystemNotificationView(props: NotificationItemTypeProps<SystemNotification>): ReactNode {
+	return (
+		<>
+			<PortalIcon href={props.notif.content.link} src={props.notif.content.imagePath}></PortalIcon>
+			<div className="body">
+				<a href={props.notif.content.link}>
+					<span className="text">
+						{props.notif.content.text}
+						<span className="timestamp">
 							{' '}
-							<T
-								k="notifications.limited_from_posting.footer"
-								values={{
-									banAppealUrl: 'https://preten.do/ban-appeal/'
-								}}
-							/>
-							<span className="timestamp">
-								{' '}
-								{humanFromNow(data.updatedAt)}
-							</span>
+							{humanFromNow(props.data.updatedAt)}
 						</span>
-					</a>
-				</div>
-			</>
-		);
+					</span>
+				</a>
+			</div>
+		</>
+	);
+}
+
+function PortalNotificationItem(props: NotificationItemProps): ReactNode {
+	const notif = props.notification.notif;
+	if (notif.type === 'follow') {
+		return <FollowNotificationView data={props.notification} notif={notif} />;
 	}
 
-	if (data.notif.type === 'system') {
-		return (
-			<>
-				<PortalIcon href={data.notif.content.link} src={data.notif.content.imagePath}></PortalIcon>
-				<div className="body">
-					<a href={data.notif.content.link}>
-						<span className="text">
-							{data.notif.content.text}
-							<span className="timestamp">
-								{' '}
-								{humanFromNow(data.updatedAt)}
-							</span>
-						</span>
-					</a>
-				</div>
-			</>
-		);
+	if (notif.type === 'postDeleted') {
+		return <PostDeletedNotificationView data={props.notification} notif={notif} />;
+	}
+
+	if (notif.type === 'limitedFromPosting') {
+		return <LimitedFromPostingNotificationView data={props.notification} notif={notif} />;
+	}
+
+	if (notif.type === 'system') {
+		return <SystemNotificationView data={props.notification} notif={notif} />;
 	}
 
 	return <div>Invalid notification type!</div>;
--- apps/juxtaposition-ui/src/services/juxt-web/views/web/notificationListView.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/web/notificationListView.tsx
@@ -6,7 +6,7 @@ import { T } from '@/services/juxt-web/views/common/components/T';
 import { humanDate, humanFromNow } from '@/util';
 import type { ReactNode } from 'react';
 import type { TranslationKey } from '@/services/juxt-web/views/common/components/T';
-import type { Notification, ShallowUser } from '@/api/generated';
+import type { FollowNotification, LimitedFromPostingNotification, Notification, PostDeletedNotification, ShallowUser, SystemNotification } from '@/api/generated';
 
 export type NotificationWrapperViewProps = {
 	children?: ReactNode;
@@ -20,156 +20,179 @@ export type NotificationItemProps = {
 	notification: Notification;
 };
 
-function WebNotificationItem(props: NotificationItemProps): ReactNode {
+export type NotificationItemTypeProps<T> = {
+	data: Notification;
+	notif: T;
+};
+
+function FollowNotificationView(props: NotificationItemTypeProps<FollowNotification>): ReactNode {
 	const url = useUrl();
-	const data = props.notification;
-	if (data.notif.type === 'follow') {
-		const NickName = ({ user }: { user: ShallowUser | null | undefined }): ReactNode => <span className="nick-name">{user?.miiName ?? 'Nobody'}</span>;
-		const users = [...data.notif.content.users].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
-		const latestUser = users[0];
+	const NickName = ({ user }: { user: ShallowUser | null | undefined }): ReactNode => <span className="nick-name">{user?.miiName ?? 'Nobody'}</span>;
+	const users = [...props.notif.content.users].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+	const latestUser = users[0];
 
-		let i18nKey: TranslationKey = 'notifications.new_follower.message/one';
-		if (users.length === 2) {
-			i18nKey = 'notifications.new_follower.message/two';
-		}
-		if (users.length === 3) {
-			i18nKey = 'notifications.new_follower.message/three';
-		}
-		if (users.length > 3) {
-			i18nKey = 'notifications.new_follower.message/multiple';
-		}
-
-		return (
-			<div className="hover">
-				<a href={`/users/${latestUser.pid}`} className="icon-container notify">
-					<img src={url.cdn(`/mii/${latestUser.pid}/normal_face.png`)} className="icon" />
-				</a>
-				<a className="body" href={`/users/${latestUser.pid}`}>
-					<span className="text">
-						<span className="link">
-							<T
-								k={i18nKey}
-								values={{
-									count: users.length,
-									count_other: Math.max(0, users.length - 2)
-								}}
-								components={{
-									follower_one: <NickName user={users[0]?.user} />,
-									follower_two: <NickName user={users[1]?.user} />
-								}}
-							/>
-						</span>
-						<span className="timestamp">
-							{' '}
-							{humanFromNow(data.updatedAt)}
-						</span>
-					</span>
-				</a>
-			</div>
-		);
+	let i18nKey: TranslationKey = 'notifications.new_follower.message/one';
+	if (users.length === 2) {
+		i18nKey = 'notifications.new_follower.message/two';
+	}
+	if (users.length === 3) {
+		i18nKey = 'notifications.new_follower.message/three';
+	}
+	if (users.length > 3) {
+		i18nKey = 'notifications.new_follower.message/multiple';
 	}
 
-	if (data.notif.type === 'postDeleted') {
-		let i18nKey: TranslationKey = 'notifications.post_deleted.post_removed';
-		if (data.notif.content.reason) {
-			i18nKey = 'notifications.post_deleted.post_removed_for_reason';
-		}
-		if (data.notif.content.postType === 'comment') {
-			i18nKey = 'notifications.post_deleted.comment_removed';
-			if (data.notif.content.reason) {
-				i18nKey = 'notifications.post_deleted.comment_removed_for_reason';
-			}
-		}
-		return (
-			<div className="hover">
-				<a href="/titles/2551084080/new" className="icon-container notify">
-					<img src="/images/bandwidthalert.png" className="icon" />
-				</a>
-				<a className="body" href="/titles/2551084080/new">
-					<span className="text">
+	return (
+		<div className="hover">
+			<a href={`/users/${latestUser.pid}`} className="icon-container notify">
+				<img src={url.cdn(`/mii/${latestUser.pid}/normal_face.png`)} className="icon" />
+			</a>
+			<a className="body" href={`/users/${latestUser.pid}`}>
+				<span className="text">
+					<span className="link">
 						<T
 							k={i18nKey}
 							values={{
-								postId: data.notif.content.postId,
-								reason: data.notif.content.reason ?? ''
+								count: users.length,
+								count_other: Math.max(0, users.length - 2)
+							}}
+							components={{
+								follower_one: <NickName user={users[0]?.user} />,
+								follower_two: <NickName user={users[1]?.user} />
 							}}
 						/>
-						{' '}
-						<T
-							k="notifications.post_deleted.footer"
-							values={{
-								contactModsUrl: 'https://preten.do/juxt-mods/'
-							}}
-						/>
-						<span className="timestamp">
-							{' '}
-							{humanFromNow(data.updatedAt)}
-						</span>
 					</span>
-				</a>
-			</div>
-		);
+					<span className="timestamp">
+						{' '}
+						{humanFromNow(props.data.updatedAt)}
+					</span>
+				</span>
+			</a>
+		</div>
+	);
+}
+
+function PostDeletedNotificationView(props: NotificationItemTypeProps<PostDeletedNotification>): ReactNode {
+	let i18nKey: TranslationKey = 'notifications.post_deleted.post_removed';
+	if (props.notif.content.reason) {
+		i18nKey = 'notifications.post_deleted.post_removed_for_reason';
+	}
+	if (props.notif.content.postType === 'comment') {
+		i18nKey = 'notifications.post_deleted.comment_removed';
+		if (props.notif.content.reason) {
+			i18nKey = 'notifications.post_deleted.comment_removed_for_reason';
+		}
 	}
 
-	if (data.notif.type === 'limitedFromPosting') {
-		let i18nKey: TranslationKey = 'notifications.limited_from_posting.message';
-		if (data.notif.content.reason) {
-			i18nKey = 'notifications.limited_from_posting.message_with_reason';
-		}
-		if (data.notif.content.until) {
-			i18nKey = 'notifications.limited_from_posting.temporary';
-			if (data.notif.content.reason) {
-				i18nKey = 'notifications.limited_from_posting.temporary_with_reason';
-			}
-		}
-		return (
-			<div className="hover">
-				<a href="/titles/2551084080/new" className="icon-container notify">
-					<img src="/images/bandwidthalert.png" className="icon" />
-				</a>
-				<a className="body" href="/titles/2551084080/new">
-					<span className="text">
-						<T
-							k={i18nKey}
-							values={{
-								until: data.notif.content.until ? humanDate(data.notif.content.until) : '',
-								reason: data.notif.content.reason ?? ''
-							}}
-						/>
+	return (
+		<div className="hover">
+			<a href="/titles/2551084080/new" className="icon-container notify">
+				<img src="/images/bandwidthalert.png" className="icon" />
+			</a>
+			<a className="body" href="/titles/2551084080/new">
+				<span className="text">
+					<T
+						k={i18nKey}
+						values={{
+							postId: props.notif.content.postId,
+							reason: props.notif.content.reason ?? ''
+						}}
+					/>
+					{' '}
+					<T
+						k="notifications.post_deleted.footer"
+						values={{
+							contactModsUrl: 'https://preten.do/juxt-mods/'
+						}}
+					/>
+					<span className="timestamp">
 						{' '}
-						<T
-							k="notifications.limited_from_posting.footer"
-							values={{
-								banAppealUrl: 'https://preten.do/ban-appeal/'
-							}}
-						/>
-						<span className="timestamp">
-							{' '}
-							{humanFromNow(data.updatedAt)}
-						</span>
+						{humanFromNow(props.data.updatedAt)}
 					</span>
-				</a>
-			</div>
-		);
+				</span>
+			</a>
+		</div>
+	);
+}
+
+function LimitedFromPostingNotificationView(props: NotificationItemTypeProps<LimitedFromPostingNotification>): ReactNode {
+	let i18nKey: TranslationKey = 'notifications.limited_from_posting.message';
+	if (props.notif.content.reason) {
+		i18nKey = 'notifications.limited_from_posting.message_with_reason';
+	}
+	if (props.notif.content.until) {
+		i18nKey = 'notifications.limited_from_posting.temporary';
+		if (props.notif.content.reason) {
+			i18nKey = 'notifications.limited_from_posting.temporary_with_reason';
+		}
 	}
 
-	if (data.notif.type === 'system') {
-		return (
-			<div className="hover">
-				<a href={data.notif.content.link} className="icon-container notify">
-					<img src={data.notif.content.imagePath} className="icon" />
-				</a>
-				<a className="body" href={data.notif.content.link}>
-					<span className="text">
-						{data.notif.content.text}
-						<span className="timestamp">
-							{' '}
-							{humanFromNow(data.updatedAt)}
-						</span>
+	return (
+		<div className="hover">
+			<a href="/titles/2551084080/new" className="icon-container notify">
+				<img src="/images/bandwidthalert.png" className="icon" />
+			</a>
+			<a className="body" href="/titles/2551084080/new">
+				<span className="text">
+					<T
+						k={i18nKey}
+						values={{
+							until: props.notif.content.until ? humanDate(props.notif.content.until) : '',
+							reason: props.notif.content.reason ?? ''
+						}}
+					/>
+					{' '}
+					<T
+						k="notifications.limited_from_posting.footer"
+						values={{
+							banAppealUrl: 'https://preten.do/ban-appeal/'
+						}}
+					/>
+					<span className="timestamp">
+						{' '}
+						{humanFromNow(props.data.updatedAt)}
 					</span>
-				</a>
-			</div>
-		);
+				</span>
+			</a>
+		</div>
+	);
+}
+
+function SystemNotificationView(props: NotificationItemTypeProps<SystemNotification>): ReactNode {
+	return (
+		<div className="hover">
+			<a href={props.notif.content.link} className="icon-container notify">
+				<img src={props.notif.content.imagePath} className="icon" />
+			</a>
+			<a className="body" href={props.notif.content.link}>
+				<span className="text">
+					{props.notif.content.text}
+					<span className="timestamp">
+						{' '}
+						{humanFromNow(props.data.updatedAt)}
+					</span>
+				</span>
+			</a>
+		</div>
+	);
+}
+
+function WebNotificationItem(props: NotificationItemProps): ReactNode {
+	const notif = props.notification.notif;
+	if (notif.type === 'follow') {
+		return <FollowNotificationView data={props.notification} notif={notif} />;
+	}
+
+	if (notif.type === 'postDeleted') {
+		return <PostDeletedNotificationView data={props.notification} notif={notif} />;
+	}
+
+	if (notif.type === 'limitedFromPosting') {
+		return <LimitedFromPostingNotificationView data={props.notification} notif={notif} />;
+	}
+
+	if (notif.type === 'system') {
+		return <SystemNotificationView data={props.notification} notif={notif} />;
 	}
 
 	return <div>Invalid notification type!</div>;
