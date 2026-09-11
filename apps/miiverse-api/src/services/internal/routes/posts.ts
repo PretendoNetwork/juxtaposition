859fd9a18b710686085d08b0cef54275368a2054
Ash Logan <ash@heyquark.com>
2026-09-11 16:02:51 +1000

    fix(api): check for self-empathy and self-replies before notifying

--- apps/miiverse-api/src/services/internal/routes/posts.ts
+++ apps/miiverse-api/src/services/internal/routes/posts.ts
@@ -271,7 +271,7 @@ postsRouter.post({
 				settings: true
 			}
 		});
-		if (targetUser?.settings?.notifyEmpathy) {
+		if (targetUser && pid !== targetUser.pid && targetUser.settings?.notifyEmpathy) {
 			await createNewEmpathyNotification(db, { currentUser: pid, postAuthor: post.pid, postId: post.id });
 		}
 
@@ -371,6 +371,7 @@ postsRouter.post({
 	},
 	async handler({ body, params, auth, db }) {
 		const account = auth!;
+		const pid = account.pnid.pid;
 
 		const parentPost = await Post.findOne({
 			id: params.post_id,
@@ -396,7 +397,7 @@ postsRouter.post({
 		}
 		const newPost = await createNewPost({
 			author: {
-				pid: account.pnid.pid,
+				pid,
 				miiData: account.pnid.mii?.data ?? '',
 				screenName: account.user?.displayName ?? '',
 				verified: self.permissions.moderator
@@ -414,7 +415,7 @@ postsRouter.post({
 				settings: true
 			}
 		});
-		if (targetUser?.settings?.notifyReply) {
+		if (targetUser && pid !== targetUser.pid && targetUser.settings?.notifyReply) {
 			await createNewReplyNotification(db, { reply: newPost, replyToUser: targetUser.pid });
 		}
 
