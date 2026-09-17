8bc7d473e232d24aff3d93e18281a493355074a3
mrjvs <jellevs@gmail.com>
2026-09-18 00:49:28 +0200

    feat: add unit tests for mention parsing

--- packages/common/src/md/plugins/mentions.test.ts
+++ packages/common/src/md/plugins/mentions.test.ts
@@ -1,6 +1,7 @@
 import { parseJuxtMarkdownInternal } from '@/md/parser';
 import { extractMentionDiscoveries, handleMentionDiscoveries } from '@/md/plugins/mention';
 import type { Token } from 'markdown-it';
+import type { TransformReplacement } from '@/md/transform';
 import type { MentionDiscovery } from '@/md/plugins/mention';
 
 describe('mention discovery', () => {
@@ -12,8 +13,6 @@ describe('mention discovery', () => {
 
 	it('handles normal text', () => test('Hello world!', []));
 	it('handles incomplete mentions', () => test('Hello @ world!', []));
-	it('doesnt discover too short mentions', () => test('Hello @abc world!', []));
-	it('doesnt discover too long mentions', () => test('Hello @thisistoolongtobeamention world!', []));
 	it('doesnt invalid character mentions', () => test('Hello @[] @<> @$asd @#avbs  world!', []));
 
 	it('discovers mentions', () => {
@@ -33,6 +32,22 @@ describe('mention discovery', () => {
 			value: '@cool2001'
 		}]);
 	});
+
+	it('discovers short mentions', () => {
+		test('Hello @a world!', [{
+			start: 6,
+			end: 9,
+			value: '@a'
+		}]);
+	});
+
+	it('discovers long mentions', () => {
+		test('Hello @jake_is_cool_and_i_wish_to_meet_him_sometime_in_this world!', [{
+			start: 6,
+			end: 60,
+			value: '@jake_is_cool_and_i_wish_to_meet_him_sometime_in_this'
+		}]);
+	});
 });
 
 describe('handleMentionDiscoveries', () => {
@@ -45,11 +60,53 @@ describe('handleMentionDiscoveries', () => {
 		return val ? { pid: val } : null;
 	}
 
-	it('handles empty input', () => {
-		expect(handleMentionDiscoveries([], pnidLookup)).toStrictEqual([]);
+	it('handles empty input', async () => {
+		expect(await handleMentionDiscoveries([], pnidLookup)).toStrictEqual([]);
 	});
 
-	// TODO add more tests
+	it('handles inavlid pnids', async () => {
+		expect(await handleMentionDiscoveries([{
+			start: 42,
+			end: 42,
+			value: '@test'
+		}], pnidLookup)).toStrictEqual([]);
+	});
+
+	it('handles valid pnids', async () => {
+		expect(await handleMentionDiscoveries([{
+			start: 42,
+			end: 42,
+			value: '@hello'
+		}], pnidLookup)).toStrictEqual<TransformReplacement[]>([{
+			start: 42,
+			end: 42,
+			value: '<@12353>'
+		}]);
+	});
+
+	it('handles multiple pnids', async () => {
+		expect(await handleMentionDiscoveries([{
+			start: 42,
+			end: 42,
+			value: '@hello'
+		}, {
+			start: 2,
+			end: 2,
+			value: '@fake'
+		}, {
+			start: 3,
+			end: 3,
+			value: '@world'
+		}], pnidLookup)).toStrictEqual<TransformReplacement[]>([{
+			start: 42,
+			end: 42,
+			value: '<@12353>'
+		}, {
+			start: 3,
+			end: 3,
+			value: '<@12361236>'
+		}]);
+	});
 });
 
 describe('mentionSyntaxRule', () => {
@@ -60,7 +117,39 @@ describe('mentionSyntaxRule', () => {
 
 	it('handles normal text', () => {
 		const val = run('Hello world!');
-		expect(val).toBeTruthy(); // TODO make real test
+		expect(val.length).toBe(3);
+		expect(val.map(v => v.type)).toStrictEqual(['paragraph_open', 'inline', 'paragraph_close']);
+		const inlineTokens = val[1].children ?? [];
+		expect(inlineTokens.length).toBe(1);
+		expect(inlineTokens[0].type).toBe('text');
+		expect(inlineTokens[0].content).toBe('Hello world!');
 	});
-	// TODO add more tests
+
+	it('handles mention syntax', () => {
+		const val = run('Hello <@1234>!');
+		expect(val.length).toBe(3);
+		expect(val.map(v => v.type)).toStrictEqual(['paragraph_open', 'inline', 'paragraph_close']);
+		const inlineTokens = val[1].children ?? [];
+		expect(inlineTokens.length).toBe(3);
+
+		const [prefix, mention, suffix] = inlineTokens;
+		expect(prefix.type).toBe('text');
+		expect(prefix.content).toBe('Hello ');
+
+		expect(mention.type).toBe('mention');
+		expect(mention.content).toBe('1234');
+
+		expect(suffix.type).toBe('text');
+		expect(suffix.content).toBe('!');
+	});
+
+	it('handles broken syntax as normal text', () => {
+		const val = run('Hello <@> <@23462346123512351235>!'); // Too short and too long
+		expect(val.length).toBe(3);
+		expect(val.map(v => v.type)).toStrictEqual(['paragraph_open', 'inline', 'paragraph_close']);
+		const inlineTokens = val[1].children ?? [];
+		expect(inlineTokens.length).toBe(1);
+		expect(inlineTokens[0].type).toBe('text');
+		expect(inlineTokens[0].content).toBe('Hello <@> <@23462346123512351235>!');
+	});
 });
