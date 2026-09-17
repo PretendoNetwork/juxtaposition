59686d7154b19a0ce57960d76b1f8e7abbb1dd36
mrjvs <jellevs@gmail.com>
2026-09-17 19:18:10 +0200

    feat: add markdown rendering to posts

--- apps/juxtaposition-ui/src/services/juxt-web/views/common/components/PostContent.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/common/components/PostContent.tsx
@@ -1,4 +1,6 @@
 import cx from 'classnames';
+import { parseJuxtMarkdown } from '@repo/common';
+import type { JuxtMdInlineNode, JuxtMdNode } from '@repo/common';
 import type { ReactNode } from 'react';
 
 export type PostContentProps = {
@@ -10,19 +12,56 @@ export type PostContentProps = {
 	post: { body: string | null; bodyMarkdown: string | null };
 };
 
+function renderInlineNodes(nodes: JuxtMdInlineNode[]): ReactNode {
+	return (
+		<>
+			{nodes.map((node) => {
+				if (node.type === 'text') {
+					return node.value;
+				}
+				if (node.type === 'br') {
+					return <br />;
+				}
+				if (node.type === 'code') {
+					return <pre className="prose-code">{node.value}</pre>;
+				}
+
+				if (node.type === 'bold') {
+					return <b className="prose-bold">{renderInlineNodes(node.children)}</b>;
+				}
+				if (node.type === 'italic') {
+					return <em className="prose-italic">{renderInlineNodes(node.children)}</em>;
+				}
+				if (node.type === 'strikethrough') {
+					return <s className="prose-strikethrough">{renderInlineNodes(node.children)}</s>;
+				}
+
+				return '';
+			})}
+		</>
+	);
+}
+
+function renderNode(node: JuxtMdNode): ReactNode {
+	if (node.type === 'paragraph') {
+		return <p className="prose-par">{renderInlineNodes(node.children)}</p>;
+	}
+}
+
 export function PostMarkdown(props: { className?: string; content: string }): ReactNode {
+	const nodes = parseJuxtMarkdown(props.content);
+
 	return (
 		<div className={props.className}>
-			<p>{props.content}</p>
+			{nodes.map(node => <>{renderNode(node)}</>)}
 		</div>
 	);
 }
 
 export function PostContent(props: PostContentProps): ReactNode {
-	// TODO disabled until markdown rendering works
-	// if (props.post.bodyMarkdown) {
-	// 	return <PostMarkdown className={cx(props.classNames?.container, props.classNames?.markdownContainer)} content={props.post.bodyMarkdown} />;
-	// }
+	if (props.post.bodyMarkdown) {
+		return <PostMarkdown className={cx(props.classNames?.container, props.classNames?.markdownContainer)} content={props.post.bodyMarkdown} />;
+	}
 
 	if (props.post.body) {
 		return (
