83da8c58ce7c202e15cbf096f9ca1ecc3e7e7f18
mrjvs <jellevs@gmail.com>
2026-09-17 17:01:35 +0200

    feat: implement plain-text rendering

--- apps/juxtaposition-ui/src/services/juxt-web/views/common/components/PostContent.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/common/components/PostContent.tsx
@@ -11,7 +11,7 @@ export type PostContentProps = {
 };
 
 export function PostMarkdown(props: { className?: string; content: string }): ReactNode {
-	return ( // TODO render markdown
+	return (
 		<div className={props.className}>
 			<p>{props.content}</p>
 		</div>
@@ -19,9 +19,10 @@ export function PostMarkdown(props: { className?: string; content: string }): Re
 }
 
 export function PostContent(props: PostContentProps): ReactNode {
-	if (props.post.bodyMarkdown) {
-		return <PostMarkdown className={cx(props.classNames?.container, props.classNames?.markdownContainer)} content={props.post.bodyMarkdown} />;
-	}
+	// TODO disabled until markdown rendering works
+	// if (props.post.bodyMarkdown) {
+	// 	return <PostMarkdown className={cx(props.classNames?.container, props.classNames?.markdownContainer)} content={props.post.bodyMarkdown} />;
+	// }
 
 	if (props.post.body) {
 		return (
--- packages/common/src/md/renderer.ts
+++ packages/common/src/md/renderer.ts
@@ -1,6 +1,46 @@
-import type { JuxtMdNode } from '@/md/tree';
+import type { JuxtMdInlineNode, JuxtMdNode } from '@/md/tree';
 
-export function renderToPlainText(_tree: JuxtMdNode[]): string {
-	// TODO implement
-	return '';
+export function renderInlineToPlainText(nodes: JuxtMdInlineNode[]): string {
+	const runs: string[] = [];
+
+	for (const node of nodes) {
+		if (node.type === 'text') {
+			runs.push(node.value);
+			continue;
+		}
+
+		if (node.type === 'br') {
+			runs.push('\n');
+			continue;
+		}
+
+		if (node.type === 'code') {
+			runs.push(node.value);
+			continue;
+		}
+
+		if (node.type === 'bold' || node.type === 'italic' || node.type === 'strikethrough') {
+			runs.push(...renderInlineToPlainText(node.children));
+			continue;
+		}
+
+		throw new Error(`Unknown node: ${node.type}`);
+	}
+
+	return runs.join('');
+}
+
+export function renderToPlainText(tree: JuxtMdNode[]): string {
+	const blocks: string[] = [];
+
+	for (const node of tree) {
+		if (node.type === 'paragraph') {
+			blocks.push(renderInlineToPlainText(node.children));
+			continue;
+		}
+
+		throw new Error(`Unknown node: ${node.type}`);
+	}
+
+	return blocks.join('\n\n');
 }
